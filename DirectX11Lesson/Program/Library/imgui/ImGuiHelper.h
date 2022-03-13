e86ee62cc0d78d76dd77c30c9f1f9494288114af
Blaec Lo <reborn1456@users.noreply.github.com>
2022-03-13 12:45:05 +0900

    Added folder
    【My Lesson Project: DirectX 11 Lesson Commit】
    ・ Added the Library/imgui folder
    ・ Added the Library/json11KdCustom folder
    ・ Added the Library/tinygltf folder
    ・ Added strconv.h

---
+++ DirectX11Lesson/Program/Library/imgui/ImGuiHelper.h
@@ -0,0 +1,34 @@
+﻿//============================================
+//ログウィンドウクラス
+//============================================
+class ImGuiLogWindow {
+public:
+	//ログをクリア
+	void Clear() { m_Buf.clear(); }
+
+	//文字列を追加
+	template<class... Args>
+	void AddLog(const char* fmt, Args... args)
+	{
+		//改行を加える
+		std::string str = fmt;
+		str += "\n";
+		m_Buf.appendf(str.c_str(), args...);
+		m_ScrollToButtom = true;
+	}
+
+	//ウィンドウ描画
+	void ImGuiUpdate(const char* title, bool* p_opende = NULL)
+	{
+		ImGui::Begin(title, p_opende);
+		ImGui::TextUnformatted(m_Buf.begin());
+		if (m_ScrollToButtom)
+			ImGui::SetScrollHere(1.0f);
+		m_ScrollToButtom = false;
+		ImGui::End();
+	}
+
+private:
+	ImGuiTextBuffer m_Buf;
+	bool m_ScrollToButtom;
+};
